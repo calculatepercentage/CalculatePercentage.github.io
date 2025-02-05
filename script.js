b1d1ebd745955e9893f5c251f5426bdf5749956f
Image Resize <ipopcornmovie@gmail.com>
2025-02-05 13:29:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function calculatePercentage() {
     let number = document.getElementById("number").value;
     let percentage = document.getElementById("percentage").value;
-    
+
     if (number === "" || percentage === "") {
         alert("Please enter both values");
         return;
@@ -9,4 +9,17 @@ function calculatePercentage() {
 
     let result = (number * percentage) / 100;
     document.getElementById("result").innerText = `${percentage}% of ${number} = ${result}`;
+}
+
+function toggleMenu() {
+    let sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "180px") {
+        sidebar.style.width = "0";
+    } else {
+        sidebar.style.width = "180px";
+    }
+}
+
+function closeMenu() {
+    document.getElementById("sidebar").style.width = "0";
 }
